32ff7f10cfeed38128e5f833105b7c489967ea78
deuole <95882894+DEUOLEIDELB@users.noreply.github.com>
2021-12-11 01:56:36 +0100

    First attempt

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("bt__fullscreen").onclick = function () {
+    document.getElementById("NFT__fullscreen").style.visibility="visible";
+};
+
+document.getElementById("NFT__fullscreen").onclick = function() {
+    document.getElementById("NFT__fullscreen").style.visibility="hidden";
+}
